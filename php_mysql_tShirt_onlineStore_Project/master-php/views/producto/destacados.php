637acd13f5669628eb6932f873c405aa10ae3bd5
MurielGN <muriel.glez@gmail.com>
2022-01-23 11:13:54 +0100

    finished php part 23_01_2022

--- php_mysql_tShirt_onlineStore_Project/master-php/views/producto/destacados.php
+++ php_mysql_tShirt_onlineStore_Project/master-php/views/producto/destacados.php
@@ -3,6 +3,7 @@
 <?php while($product = $productos->fetch_object()): ?>
 	<div class="product">
 		<a href="<?=base_url?>producto/ver&id=<?=$product->id?>">
+
 			<?php if($product->imagen != null): ?>
 				<img src="<?=base_url?>uploads/images/<?=$product->imagen?>" />
 			<?php else: ?>
@@ -14,14 +15,14 @@
 		<?php if($product->descuento == 0):?>
 			<p><?= $product->precio ?></p>
 		<?php else: ?>
-			<p class="ofertaNueva"><?= $product->precio * (100 - $product->descuento )/100 ?></p>
-			<p class="ofertaAntigua"><?= $product->precio ?></p>
+			<p class="ofertaNueva"><?= $product->precio * (100 - $product->descuento )/100 ?>
+			<span class="ofertaAntigua"><?= $product->precio ?></span></p>
 		<?php endif; ?>
 
 		<?php if($product->stock > 0):?>
 			<a href="<?=base_url?>carrito/add&id=<?=$product->id?>" class="button">Comprar</a>
 			<?php else: ?>
-				<a" class="button button-gestion button-red">SIN STOCK</a>
+				<span class="sinStock">SIN STOCK</span>
 			<?php endif; ?>
 		
 	</div>
